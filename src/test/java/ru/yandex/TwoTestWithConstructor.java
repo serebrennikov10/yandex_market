59c070515a39c023e44896217ac04a92ac79ca94
serebrennikov10 <serebrennikov10@mail.ru>
2019-09-22 22:13:31 +0300

    fix TwoTest + constructor

--- src/test/java/ru/yandex/TwoTestWithConstructor.java
+++ src/test/java/ru/yandex/TwoTestWithConstructor.java
@@ -3,24 +3,38 @@ package ru.yandex;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 
 import java.util.List;
 
+public class TwoTestWithConstructor extends WebDriverSetting {
 
-public class TwoTest extends WebDriverSetting {
-    //Actions MoveToElement = new Actions(driver);
     public class Note {
         String timeWork, valueBattery, valueBatteryPower, cells, typeBattery;
 
+        public Note(String timeWork, String valueBattery){
+            this.timeWork = timeWork;
+            this.valueBattery = valueBattery;
+/*            this.valueBatteryPower = valueBatteryPower;
+            this.cells = cells;
+            this.typeBattery = typeBattery;*/
 
+        }
+
+        public Note(String timeWork, String valueBattery, String valueBatteryPower){
+            this.timeWork = timeWork;
+            this.valueBattery = valueBattery;
+            this.valueBatteryPower = valueBatteryPower;
+/*            this.cells = cells;
+            this.typeBattery = typeBattery;*/
+
+        }
 
     }
 
-    private void setAttributes(Note note){
-
+    public void setAttributes(Note note){
+        //String timeWork, String valueBattery, String valueBatteryPower, String cells, String typeBattery
         String timeWork = null;
         String valueBattery = null;
         String valueBatteryPower = null;
@@ -70,7 +84,9 @@ public class TwoTest extends WebDriverSetting {
                 typeBattery = "Тип аккумулятора: Нет значения";
                 System.out.println(typeBattery);
             }
+
         }
+
 
 
         note.timeWork = timeWork;
@@ -79,9 +95,10 @@ public class TwoTest extends WebDriverSetting {
         note.cells = cells;
         note.typeBattery = typeBattery;
 
-        //return note;
-    }
+        System.out.println("--------------------------------------");
 
+return;
+    }
 
     @Test
     public void main(){
@@ -96,17 +113,26 @@ public class TwoTest extends WebDriverSetting {
         marketPage.selectNote(0);
         marketPage.openNoteSpec();
 
-        Note note1 = new Note();
+        String timeWork = "Пока еще нет значения";
+        String valueBattery = "Пока еще нет значения";
+        String valueBatteryPower = "Пока еще нет значения";
+        String cells = "Пока еще нет значения";
+        String typeBattery = "Пока еще нет значения";
+
+
+        Note note1 = new Note(timeWork, valueBattery);
+        setAttributes(note1);
         //note1.timeWork = "note1";
 
         //System.out.println("note1 до изменения: "+note1);
-        setAttributes(note1);
+       // setAttributes(note1);
         System.out.println("note1 после: ");
         System.out.println(note1.timeWork);
         System.out.println(note1.valueBattery);
         System.out.println(note1.valueBatteryPower);
         System.out.println(note1.cells);
         System.out.println(note1.typeBattery);
+        System.out.println("--------------------------------------");
 
         marketPage.openPage();
         marketPage.openAllCategories();
@@ -117,7 +143,7 @@ public class TwoTest extends WebDriverSetting {
         marketPage.selectNote(1);
         marketPage.openNoteSpec();
 
-        Note note2 = new Note();
+        Note note2 = new Note(timeWork, valueBattery, valueBatteryPower);
         //note2.timeWork = "note2";
         //System.out.println("note2 до изменения: "+note2.timeWork);
         setAttributes(note2);
@@ -127,10 +153,10 @@ public class TwoTest extends WebDriverSetting {
         System.out.println(note2.valueBatteryPower);
         System.out.println(note2.cells);
         System.out.println(note2.typeBattery);
+        System.out.println("--------------------------------------");
 
-
-        System.out.println("Итого note1 после: "+note1.timeWork);
-        System.out.println("Итого note2 после: "+note2.timeWork);
+        //System.out.println("Итого note1 после: "+note1.timeWork);
+        //System.out.println("Итого note2 после: "+note2.timeWork);
         System.out.println("Сравниваю характеристики блока 'Питание': ");
         System.out.println("Время работы: "+note1.timeWork.equalsIgnoreCase(note2.timeWork));
         System.out.println("Емкость аккумулятора: "+note1.valueBattery.equalsIgnoreCase(note2.valueBattery));
@@ -145,40 +171,6 @@ public class TwoTest extends WebDriverSetting {
 
 
 
-
-
-//@Test
-    public void out(){
-        MarketPage marketPage = PageFactory.initElements(driver, MarketPage.class);
-        marketPage.openPage();
-        marketPage.openAllCategories();
-        marketPage.openCompCategory();
-        marketPage.openNotebookCategory();
-        marketPage.sortByPrice();
-        marketPage.sortPriceByAsc();
-        marketPage.selectNote(0);
-        marketPage.openNoteSpec();
-
-    System.out.println("Ищу поле с подсказкой");
-    //WebElement help = driver.findElement(By.className("link link_pseudo_yes n-hint-button__handle i-bem link_js_inited _popup-destructor _popup-destructor_js_inited"));
-    WebElement help = driver.findElement(By.xpath("/html/body/div[1]/div[6]/div[1]/.//*[text()='?']/.."));
-    //MoveToElement.moveToElement(help).perform();
-    System.out.println("кликаю");
-    help.click();
-
-    System.out.println("Ищу текст");
-    //WebElement hintText = driver.findElement(By.className("n-hint-button__inner"));
-    //WebElement hintText = driver.findElement(By.xpath("//div[@class='popup__content']"));
-    WebElement hintText = driver.findElement(By.xpath("/html/body/div[3]/div/div/div"));
-
-    /*List <WebElement> hintText = driver.findElements(By.xpath("/html/body/div[3]/div/div/div"));
-    hintText.size();*/
-    System.out.println("Вывожу информацию:");
-    System.out.println(hintText.getText());
-    //System.out.println(hintText.getText());
-
-
-    }
 
 
 }
